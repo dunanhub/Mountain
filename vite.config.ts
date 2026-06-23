0d070923f618a78badcbb6d0507974a1dc0faa80
dunanhub <ubniyev.b@gmail.com>
2026-06-24 00:37:43 +0500

    add pwa install

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,18 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/Mountain/',
         scope: '/Mountain/',
-        icons: []
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ]
